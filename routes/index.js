9cb0cbc20035ed22d2fa4b31d6b7bea979fea4b9
jinky32 <jinky32@gmail.com>
2017-04-17 21:53:31 +0100

    changed render engine to handlebars and implemented bootstrap and jquery

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Stuart Express' });
+  res.render('index', {
+    title: 'Stuart Express',
+    name: 'Stuart',
+    condition:false,
+    anyArray:[1,2,3]
+  });
+});
+
+router.get('/foo', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
