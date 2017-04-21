a6ea7245b0e59739a39a2ec46b29a7dba49418ae
jinky32 <jinky32@gmail.com>
2017-04-21 20:13:06 +0100

    idy.  Need to figure out why not all json objects are being imported

--- seed/statement-seeder.js
+++ seed/statement-seeder.js
@@ -12,19 +12,22 @@ console.log('ready state is '+mongoose.connection.readyState);
 var parseDate = require('../helpers/parseDate');
 //console.log(statements);
 
-
+//TODO figure out why all records from the json aren't being imported
 var done = 0;
 for( var i = 0; i < statements.length; i++ ) {
     var newStatement = new Statement();
     newStatement.date = parseDate.stringToDate(statements[i].FIELD1);
     newStatement.name = statements[i].FIELD2;
     newStatement.amount = Number(statements[i].FIELD3);
+    //console.log('imported record ' + i + 'of ' + statements.length);
     //newStatement.save();
-
     newStatement.save(function(err, result){
         done++;
         if(done === statements.length){
             exit();
+        }
+        else {
+            console.log('imported '+ statements[done].FIELD2)
         }
     });
 
