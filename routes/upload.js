0a8065126ca83d353ca73c75190aafe9b3870c3b
jinky32 <jinky32@gmail.com>
2017-04-25 17:23:37 +0100

    added a todo related to showing a success message on upload

--- routes/upload.js
+++ routes/upload.js
@@ -24,13 +24,6 @@ var storage = multer.diskStorage({
     }
 });
 
-//router.post('/', multer({ des: './data/' }).single('statement'), function(req,res){
-//    console.log(req.body); //form fields
-//
-//    console.log(req.file); //form files
-//
-//    res.status(204).end();
-//});
 
 router.post('/',
     multer({
@@ -50,79 +43,10 @@ router.post('/',
         console.log(req.body.bank, 'Body');
 
         console.log(req.files, 'files');
-        res.end('File is uploaded')
+        //res.end('File is uploaded')
+        res.send('File is uploaded')
 
+//TODO need to find a way to send a success message on successful upload
 });
 
-//router.post('/', function(req, res) {
-//    console.log(req.body, 'Body');
-//    console.log(req.files, 'files');
-//    var upload = multer({
-//        storage: storage,
-//        fileFilter: function(req, file, callback) {
-//            var ext = path.extname(file.originalname)
-//            if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-//                return callback(res.end('Only images are allowed'), null)
-//            }
-//            callback(null, true)
-//        }
-//    }).single('statement');
-//    upload(req, res, function(err) {
-//        res.end('File is uploaded')
-//    })
-//})
-
-
-
-
-//
-//
-//var express = require('express');
-//
-//var multer = require('multer');
-//var storage = multer.diskStorage({
-//    destination: './data/',
-//    filename: function (req, file, cb) {
-//        crypto.pseudoRandomBytes(16, function (err, raw) {
-//            if (err) return cb(err)
-//
-//            cb(null, raw.toString('hex') + path.extname(file.originalname))
-//        })
-//    }
-//});
-//
-////var upload = multer({ storage: storage });
-//
-//var path = require('path')
-////var multer = require('multer')
-//
-//
-//
-////Get root route
-//router.get('/', function(req, res, next) {
-//
-//    res.render('upload');
-//});
-//
-////router.post('/', upload.any(), function(req, res, next){
-////    //res.send(req.files)
-////    console.log(req.body, 'Body');
-////    console.log(req.files, 'files');
-////    res.end();
-////});
-//
-//router.post('/', multer({ storage: storage }).single('statement'), function(req,res){
-//    console.log(req.body); //form fields
-//
-//    console.log(req.file); //form files
-//
-//    res.status(204).end();
-//});
-
-
-//router.post('/saveBlog', upload.any(), function(req, res, next) {
-//    console.log(req.body, 'Body');
-//    console.log(req.files, 'files');
-//    res.end();
-//});
 module.exports = router;
