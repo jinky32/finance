252128e1cabdd970eeaf55a92b0611f4a4331d34
jinky32 <jinky32@gmail.com>
2017-04-25 16:10:45 +0100

    upload and rename via multer added but more work to do!

--- routes/upload.js
+++ routes/upload.js
@@ -2,22 +2,96 @@
  * Created by stuartbrown on 25/04/2017.
  */
 
-var express = require('express');
+var express = require("express");
+var multer = require('multer');
 var router = express.Router();
+var path = require('path');
+
 
 //Get root route
 router.get('/', function(req, res, next) {
 
-    res.render('upload', {
-        vendor: 'test'
-        //shop:doc
-    });
+    res.render('upload');
 });
-//        res.render('upload', {
-//            //vendor: vendorName,
-//            //shop:doc
-//        });
-//    });
+
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './data')
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+
+router.post('/', function(req, res) {
+    var upload = multer({
+        storage: storage,
+        fileFilter: function(req, file, callback) {
+            var ext = path.extname(file.originalname)
+            if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+                return callback(res.end('Only images are allowed'), null)
+            }
+            callback(null, true)
+        }
+    }).single('statement');
+    upload(req, res, function(err) {
+        res.end('File is uploaded')
+    })
+        console.log(req.body, 'Body');
+        console.log(req.files, 'files');
+})
+
+
+
+
+//
+//
+//var express = require('express');
+//
+//var multer = require('multer');
+//var storage = multer.diskStorage({
+//    destination: './data/',
+//    filename: function (req, file, cb) {
+//        crypto.pseudoRandomBytes(16, function (err, raw) {
+//            if (err) return cb(err)
+//
+//            cb(null, raw.toString('hex') + path.extname(file.originalname))
+//        })
+//    }
+//});
+//
+////var upload = multer({ storage: storage });
+//
+//var path = require('path')
+////var multer = require('multer')
+//
+//
+//
+////Get root route
+//router.get('/', function(req, res, next) {
+//
+//    res.render('upload');
+//});
+//
+////router.post('/', upload.any(), function(req, res, next){
+////    //res.send(req.files)
+////    console.log(req.body, 'Body');
+////    console.log(req.files, 'files');
+////    res.end();
+////});
+//
+//router.post('/', multer({ storage: storage }).single('statement'), function(req,res){
+//    console.log(req.body); //form fields
+//
+//    console.log(req.file); //form files
+//
+//    res.status(204).end();
 //});
 
+
+//router.post('/saveBlog', upload.any(), function(req, res, next) {
+//    console.log(req.body, 'Body');
+//    console.log(req.files, 'files');
+//    res.end();
+//});
 module.exports = router;
---
+++ routes/upload_old.js
@@ -0,0 +1,55 @@
+/**
+ * Created by stuartbrown on 25/04/2017.
+ */
+/**
+ * Created by stuartbrown on 25/04/2017.
+ */
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: './data/',
+    filename: function (req, file, cb) {
+        crypto.pseudoRandomBytes(16, function (err, raw) {
+            if (err) return cb(err)
+
+            cb(null, raw.toString('hex') + path.extname(file.originalname))
+        })
+    }
+});
+
+//var upload = multer({ storage: storage });
+
+var path = require('path')
+//var multer = require('multer')
+
+
+
+//Get root route
+router.get('/', function(req, res, next) {
+
+    res.render('upload');
+});
+
+//router.post('/', upload.any(), function(req, res, next){
+//    //res.send(req.files)
+//    console.log(req.body, 'Body');
+//    console.log(req.files, 'files');
+//    res.end();
+//});
+
+router.post('/', multer({ storage: storage }).single('statement'), function(req,res){
+    console.log(req.body); //form fields
+
+    console.log(req.file); //form files
+
+    res.status(204).end();
+});
+
+
+//router.post('/saveBlog', upload.any(), function(req, res, next) {
+//    console.log(req.body, 'Body');
+//    console.log(req.files, 'files');
+//    res.end();
+//});
+module.exports = router;
