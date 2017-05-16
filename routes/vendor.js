0a03ebcbff65243a52b9ee1b5adb615910c21f03
jinky32 <jinky32@gmail.com>
2017-05-16 16:20:04 +0100

    starting to work in new SO answer

--- routes/vendor.js
+++ routes/vendor.js
@@ -35,99 +35,108 @@ router.get('/:vendor?', function(req, res, next) {
 
 router.get('/:vendor/:year/:month', function(req, res, next) {
 
-    Statement.aggregate([
-        { "$match": { "name": req.params.vendor } },
-        {
-            "$redact": {
-                "$cond": [
-                    {
-                        "$and": [
-                            { "$eq": [{ "$year": "$date" },  parseInt(req.params.year)  ]},
-                            { "$eq": [{ "$month": "$date" }, parseInt(req.params.month) ]}
-                            //,
-                            //{ "$eq": [{ "$week": "$date" },  parseInt(req.params.week)  ]}
-                        ]
-                    },
-                    "$$KEEP",
-                    "$$PRUNE"
-                ]
-            }
-        },
-        {
-            "$group": {
-                "_id": {
-                    "name": "$name",
-                    "year": { "$year": "$date" },
-                    "month": { "$month": "$date" },
-                    "week": { "$week": "$date" }
-                },
-                "total": { "$sum": "$amount" }
-            }
-        },
-        {
-            "$group": {
-                "_id": {
-                    "name": "$_id.name",
-                    "year": "$_id.year"
-                },
-                "YearlySpends": { "$push": "$total" },
-                "totalYearlyAmount": { "$sum": "$total" },
-                "data": { "$push": "$$ROOT" }
-            }
-        },
-        { "$unwind": "$data" },
-        {
-            "$group": {
-                "_id": {
-                    "name": "$_id.name",
-                    "month": "$data._id.month"
-                },
-                "YearlySpends": { "$first": "$YearlySpends" },
-                "totalYearlyAmount": { "$first": "$totalYearlyAmount" },
-                "MonthlySpends": { "$push": "$data.total" },
-                "totalMonthlyAmount": { "$sum": "$data.total" },
-                "data": { "$push": "$data" }
-            }
-        },
-        { "$unwind": "$data" },
-        {
-            "$group": {
-                "_id": {
-                    "name": "$_id.name",
-                    "week": "$data._id.week"
-                },
-                "YearlySpends": { "$first": "$YearlySpends" },
-                "totalYearlyAmount": { "$first": "$totalYearlyAmount" },
-                "MonthlySpends": { "$first": "$MonthlySpends" },
-                "totalMonthlyAmount": { "$first": "$totalMonthlyAmount" },
-                "WeeklySpends": { "$push": "$data.total" },
-                "totalWeeklyAmount": { "$sum": "$data.total" },
-                "data": { "$push": "$data" }
-            }
-        },
-        { "$unwind": "$data" },
-        {
-            "$group": {
-                "_id": "$data._id",
-                "YearlySpends": { "$first": "$YearlySpends" },
-                "totalYearlyAmount": { "$first": "$totalYearlyAmount" },
-                "MonthlySpends": { "$first": "$MonthlySpends" },
-                "totalMonthlyAmount": { "$first": "$totalMonthlyAmount" },
-                "WeeklySpends": { "$first": "$WeeklySpends" },
-                "totalWeeklyAmount": { "$first": "$totalWeeklyAmount" }
-            }
-        }
-    ], function(err, data){
-        if (err) throw err;
-        var first = data[0];
-        //console.log(util.inspect(data));
-        console.log(util.inspect(first));
-        res.render('vendor-monthly', {
-            //data: data
-            data: first
-        });
-    })
 
+    // Sample request
+    var request = {
+        "name":"RINGGO",
+        "year": 2017,
+        "month":3,
+        "week":12
+    };
+
+// Build initial match document on name
+
+    var match1 = {
+        name: request["name"]
+    };
+
+// Build project & facet document for date based aggregation
+
+    var addFields = {};
+    var facet = {};
+
+// Add year followed by year facet
+
+    if (request["year"]) {
+        addFields["year"] = { "$year": "$date" },
+            facet["Yearly"] =
+                [
+                    {
+                        "$match":{ "year": request["year"] }
+                    },
+                    {
+                        "$group": {
+                            "_id": {
+                                "name": "$name",
+                                "year": "$year"
+                            },
+                            "spend": { "$push":"$amount" },
+                            "total": { "$sum": "$amount" }
+                        }
+                    }
+                ];
+    }
+
+// Add month followed by month facet
+
+    if (request["month"]) {
+        addFields["month"] = { "$month": "$date" };
+        facet["Monthly"] =
+            [
+                {
+                    "$match":{ "month": request["month"] }
+                },
+                {
+                    "$group": {
+                        "_id": {
+                            "name": "$name",
+                            "month": "$month"
+                        },
+                        "spend": { "$push":"$amount" },
+                        "total": { "$sum": "$amount" }
+                    }
+                }
+            ];
+    }
+
+// Add week followed by week facet
+
+    if (request["week"]) {
+        addFields["week"] = { "$week": "$date" };
+        facet["Weekly"] =
+            [
+                {
+                    "$match":{ "week": request["week"] }
+                },
+                {
+                    "$group": {
+                        "_id": {
+                            "name": "$name",
+                            "week": "$week"
+                        },
+                        "spend": { "$push":"$amount" },
+                        "total": { "$sum": "$amount" }
+                    }
+                }
+            ];
+    }
+
+// Use aggregate builder
+
+    Statement.aggregate()
+        .match(match1)
+        .append({"$addFields": addFields}) // No addFields stage in mongoose builder
+        .facet(facet)
+        .exec(function(err, data) {
+            if (err) throw err;
+            //console.log(data.Yearly);
+            console.log(util.inspect(data));
+            //console.log(util.inspect(first));
+            res.render('vendor-monthly', {
+                data: data
+                //data: first
+            });
+        });
 
 
 });
